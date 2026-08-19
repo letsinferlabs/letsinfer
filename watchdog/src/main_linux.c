5c92e58531f4ee80f5a3f012d25d70184b7ac5a4
taimur <timonea.dev@gmail.com>
2026-08-19 14:25:49 -0400

    Promote core 0.11.0-rc.11

--- watchdog/src/main_linux.c
+++ watchdog/src/main_linux.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define WATCHDOG_VERSION "0.11.0-rc.10"
+#define WATCHDOG_VERSION "0.11.0-rc.11"
 
 static void usage(FILE *stream, const char *program) {
     fprintf(stream,
